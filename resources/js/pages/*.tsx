135cd3178032e26ca4b717792c31bbe5252a36d3
MeKHell <mgreub@student.ethz.ch>
2025-09-18 16:09:01 +0200

    Switch to Laravel folder structure

--- resources/js/pages/*.tsx
+++ resources/js/pages/*.tsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <section class="text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">404: Not Found</h1>
-      <p class="mt-4">It's gone 😞</p>
-    </section>
+    <div class="min-h-screen">
+      <section class="text-gray-700 px-8 py-20">
+        <h1 class="text-2xl font-bold">404: Not Found</h1>
+        <p class="mt-4">It's gone 😞</p>
+      </section>
+    </div>
   );
 }
