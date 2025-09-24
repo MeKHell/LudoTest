b0d41e8a6c8766e95ccae8559c6a8ce0171d7e65
MeKHell <mgreub@student.ethz.ch>
2025-09-24 13:27:49 +0200

    Add navbar

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -1,5 +1,6 @@
 import { JSX } from "solid-js"
 import { Title } from "@solidjs/meta"
+import { NavBar } from "@/components/navbar";
 
 interface GuestLayoutProps {
     title?: string;
@@ -9,12 +10,14 @@ interface GuestLayoutProps {
 function GuestLayout(props: GuestLayoutProps) {
     return (
         <>
-            <Title>{props.title ? `${props.title} - Laravel` : 'Laravel'}</Title>
+            <Title>{props.title ? `${props.title} - LudoTest` : 'LudoTest'}</Title>
             <link rel="preconnect" href="https://fonts.bunny.net" />
             <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             
-            <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a] dark:text-[#EDEDEC]">
-                {props.children}
+            <div class="flex min-h-screen flex-col items-center lg:justify-center lg:p-8 bg-background">
+                <NavBar />
+      <main>{props.children}</main>
+ 
             </div>
         </>
     );
