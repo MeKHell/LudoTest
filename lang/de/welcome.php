1773980357f3fd34a484e4cdd0923e37cbb3003a
MeKHell <mgreub@student.ethz.ch>
2025-11-11 22:30:44 +0100

    Format lang

--- lang/de/welcome.php
+++ lang/de/welcome.php
@@ -1,16 +1,15 @@
 <?php
 
-return [  
-  "home" => "Startseite",
-  "games" => "Spiele",
-  "login" => "Anmelden",
-  "admin" => "Administrator",
-  "profile" => "Profil",
-  "logout" => "Abmelden",
-
-  "title" => "Brettspiel-Bewertungsplattform",
-  "subtitle" => "Entdecken, bewerten und rezensieren Sie Ihre Lieblings-Brettspiele",
-  "placeholder" => "Nach Spielen suchen...",
-  "featured" => "Empfohlene Spiele",
-  "popular" => "Beliebt Diese Woche",
+return [
+  'home' => 'Startseite',
+  'games' => 'Spiele',
+  'login' => 'Anmelden',
+  'admin' => 'Administrator',
+  'profile' => 'Profil',
+  'logout' => 'Abmelden',
+  'title' => 'Brettspiel-Bewertungsplattform',
+  'subtitle' => 'Entdecken, bewerten und rezensieren Sie Ihre Lieblings-Brettspiele',
+  'placeholder' => 'Nach Spielen suchen...',
+  'featured' => 'Empfohlene Spiele',
+  'popular' => 'Beliebt Diese Woche',
 ];
