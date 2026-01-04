a2043cdec2d9ca59aac2f46676aac0a351a47604
MeKHell <mgreub@student.ethz.ch>
2026-01-04 11:11:58 +0100

    Seeds the languages in the database

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LanguageSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('languages')->insert(['code' => 'FR',
+                'bgg_index' => '2187',
+                'name' => 'Français']);
+        DB::table('languages')->insert(['code' => 'EN',
+                'bgg_index' => '2184',
+                'name' => 'English',]);
+        DB::table('languages')->insert(['code' => 'DE',
+                'bgg_index' => '2188',
+                'name' => 'Deutsch']);
+        DB::table('languages')->insert(['code' => 'IT',
+                'bgg_index' => '2193',
+                'name' => 'Italiano']);
     }
 }
