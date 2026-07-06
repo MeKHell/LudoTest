f84c8fbe1b569fd090134eb3a0d6bebdded671aa
MeKHell <mgreub@student.ethz.ch>
2026-07-06 08:45:18 +0200

    Update language files to accomodate translations

--- lang/fr/welcome.php
+++ lang/fr/welcome.php
@@ -4,6 +4,13 @@ return [
   'title' => 'Plateforme de Notation de Jeux de Société',
   'subtitle' => 'Découvrez, notez et critiquez vos jeux de société préférés',
   'search.placeholder' => 'Rechercher des jeux...',
-  'featured' => 'Jeux en Vedette',
-  'popular' => 'Populaire Cette Semaine'
+  'placeholder' => 'Rechercher des jeux...',
+  'history_title' => 'Historique récent',
+  'history_subtitle' => 'Jeux avec les derniers votes et commentaires de la communauté',
+  'no_recent_games' => 'Aucune activité récente pour le moment',
+  'questions_title' => 'Meilleurs jeux par question',
+  'questions_subtitle' => 'Les jeux les mieux notés pour chaque question de la communauté',
+  'top_games_for_question' => 'Top 5 jeux',
+  'no_questions_yet' => 'Aucune question disponible pour le moment',
+  'no_answers_yet' => 'Pas encore de votes pour cette question',
 ];
